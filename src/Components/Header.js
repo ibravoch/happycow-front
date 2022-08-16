8834d5527e7bbe4fb0c6aca4b88963dd6af1506e
daniel savisky <veroullage@hotmail.fr>
2022-08-16 15:42:34 +0200

    first deploy

---
+++ src/Components/Header.js
@@ -0,0 +1,27 @@
+import happycow from "../assets/happycow.svg";
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <div className="happycow-header">
+      <img src={happycow} />
+      <nav>
+        <section>
+          <li>Explore</li>
+          <li>Forum</li>
+          <li>Blog</li>
+          <li>Community</li>
+          <li>The App</li>
+          <li>Shop</li>
+          <li>More</li>
+        </section>
+      </nav>
+      <ul>
+        <li>Add Listing</li>
+        <li>Login/signIn</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
